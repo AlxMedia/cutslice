f78dd8aa63bbd77b46ebddd35ca137046d523f15
Alexander Agnarson <contact@alx.media>
2025-04-09 20:21:47 +0200

    Kirki deprecated fix

--- functions.php
+++ functions.php
@@ -730,3 +730,16 @@ function cutslice_skip_link_focus_fix() {
 	<?php
 }
 add_action( 'wp_print_footer_scripts', 'cutslice_skip_link_focus_fix' );
+
+
+/*  Kirki deprecated fix
+/* ------------------------------------ */
+function cutslice_kirki_config( $config ) {
+
+	if ( isset( $config['compiler'] ) ) {
+		unset( $config['compiler'] );
+	}
+
+	return $config;
+}
+add_filter( 'kirki/config', 'cutslice_kirki_config', 999 );
